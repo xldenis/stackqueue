65d4481a74a48ade30c94ba0f3d760df7fb1e0bf
Chris Bowal <cbowal@gmail.com>
2013-11-08 21:47:38 -0500

    working

--- custom.js
+++ custom.js
@@ -1,7 +1,16 @@
 
-function stackQueueCtrl($scope) {
+function stackQueueCtrl($scope, $http, $window) {
 
   $scope.title = "This is the title";
   $scope.question = "This is the question";
+
+    // load the data for the 'python' stackoverflow questions
+    $http.jsonp('https://api.stackexchange.com/2.1/search?pagesize=100&order=desc&min=50&sort=votes&tagged=python&site=stackoverflow&callback=JSON_CALLBACK')
+	  .success(function(data, status, headers, config) {
+	    	     $window.alert(data.items);
+		}).
+	    error(function(data, status, headers, config) {
+	    	     $window.alert('ERROR LOADING DATA');
+		});
 
 };
