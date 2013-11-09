09ef813d51376ceab55d477458e8fe2f3ca9c33c
Chris Bowal <cbowal@gmail.com>
2013-11-08 21:46:19 -0500

    changes

--- custom.js
+++ custom.js
@@ -1,30 +1,7 @@
-var app = angular.module( 'myApp', [] );
 
-app.controller( 'MainCtrl', function( $scope ) {
-  
-	$scope.init = function () {
-	    // load the data for the 'python' stackoverflow questions
-	    $http.jsonp('https://api.stackexchange.com/2.1/search?pagesize=100&order=desc&min=50&sort=votes&tagged=python&site=stackoverflow&callback=JSON_CALLBACK')
-		  .success(function(data, status, headers, config) {
-		    	     $window.alert(data.items);
-			}).
-		    error(function(data, status, headers, config) {
-		    	     $window.alert('ERROR LOADING DATA');
-			});
-		};
-
-<<<<<<< HEAD
-
-
-
-
-
-});
-=======
 function stackQueueCtrl($scope) {
 
   $scope.title = "This is the title";
   $scope.question = "This is the question";
 
-}
->>>>>>> ae9460c20bd4941e7cecb408a99db3fa5dabce30
+};
