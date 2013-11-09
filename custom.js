bec13e6269049ded516e0732a5e71ae13bf5ab3a
Chris Bowal <cbowal@gmail.com>
2013-11-08 21:44:33 -0500

    merging into angular

--- custom.js
+++ custom.js
@@ -1,5 +1,21 @@
-function processResponse(data) {
-	alert(data['items']);
-}
+var app = angular.module( 'myApp', [] );
+
+app.controller( 'MainCtrl', function( $scope ) {
+  
+	$scope.init = function () {
+	    // load the data for the 'python' stackoverflow questions
+	    $http.jsonp('https://api.stackexchange.com/2.1/search?pagesize=100&order=desc&min=50&sort=votes&tagged=python&site=stackoverflow&callback=JSON_CALLBACK')
+		  .success(function(data, status, headers, config) {
+		    	     $window.alert(data.items);
+			}).
+		    error(function(data, status, headers, config) {
+		    	     $window.alert('ERROR LOADING DATA');
+			});
+		};
 
 
+
+
+
+
+});
