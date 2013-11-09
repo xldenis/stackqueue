1c59c45f483a25141bfa310f842c5ad60fcda267
Chris Bowal <cbowal@gmail.com>
2013-11-08 22:06:18 -0500

    ajs

--- custom.js
+++ custom.js
@@ -1,13 +1,32 @@
 
 function stackQueueCtrl($scope, $http, $window) {
 
+  questions = [];
+  tags = {};
+
+
+  function processNewlyFetchedQuestions(newQuestions) {
+  	//add any new tags
+	//TODO: Cody
+
+  	//add new questions
+  	questions.push(newQuestions);
+
+  	alert(newQuestions);
+  };
+
+  function chooseBestQuestion() {
+  	
+  };
+
+
   $scope.title = "This is the title";
   $scope.question = "This is the question";
 
-    // load the data for the 'python' stackoverflow questions
-    $http.jsonp('https://api.stackexchange.com/2.1/search?pagesize=100&order=desc&min=50&sort=votes&tagged=python&site=stackoverflow&callback=JSON_CALLBACK')
+  // load the data for the 'python' stackoverflow questions
+  $http.jsonp('https://api.stackexchange.com/2.1/search?pagesize=100&order=desc&min=50&sort=votes&tagged=python&site=stackoverflow&callback=JSON_CALLBACK')
 	  .success(function(data, status, headers, config) {
-	    	     $window.alert(data.items);
+	    	     processNewlyFetchedQuestions(data.items);
 		}).
 	    error(function(data, status, headers, config) {
 	    	     $window.alert('ERROR LOADING DATA');
